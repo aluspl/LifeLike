5e923c6166eba5e65606ead23c319210a25f0cff
szymonmotyka@gmail.com <szymonmotyka@gmail.com>
2017-06-28 00:18:55 +0200

    Modified Video

--- LifeLIke/Utils/HtmlUtils.cs
+++ LifeLIke/Utils/HtmlUtils.cs
@@ -175,8 +175,8 @@ namespace LifeLike.Utils
         };
         public static string GenerateYoutube(LinkViewModel videoLink)
         {
-            var format = string.Format("<div class='embed-responsive embed-responsive-16by9'>"
-                                       +                         "<h2>{0}</h2>"+                                   
+            var format = string.Format("<h2>{0}</h2>"+
+                                       "<div class='embed-responsive embed-responsive-16by9'>" +
                                        "<iframe width='560' height='315' src='https://www.youtube.com/embed/{1}' " +
                                        "?rel'0' frameborder='0' allowfullscreen></iframe>" + 
                                        "</div>", videoLink.Name, videoLink.Link);
