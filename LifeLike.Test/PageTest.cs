d7ee531a65c498f96085ad7915dff98ab7766647
szymonmotyka@gmail.com <szymonmotyka@gmail.com>
2018-03-30 00:53:58 +0200

    Added Test - removed migrations

---
+++ LifeLike.Test/PageTest.cs
@@ -0,0 +1,30 @@
+﻿using LifeLike.Repositories;
+using Xunit;
+
+namespace LifeLike.Test
+{
+    public class PageTest
+    {
+        private IPageRepository _pageRepository;
+
+        public PageTest()
+        {
+        }
+
+        [Fact]
+        public void AnyPage()
+        {
+            Assert.Equal(1,1);
+
+        }
+        [Theory]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(6)]
+        public void PassingTest(int value)
+        {
+            Assert.Equal(4, value);
+        }
+
+    }
+}
