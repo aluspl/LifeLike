860b627db1014fb408d057dbe354e2c02e22a9e2
Szymon Motyka <szymonmotyka@gmail.com>
2019-01-21 21:15:23 +0100

    Created Self-SErviced  Frontend

--- LifeLike.Frontend/src/app/app.module.ts
+++ LifeLike.Frontend/src/app/app.module.ts
@@ -1,38 +1,36 @@
-
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-
-import {HttpClientModule, HttpClientXsrfModule} from '@angular/common/http';
-import {HomeComponent} from './Pages/home/home.component';
-import {LogComponent} from './Pages/log/log.component';
-import {PostDetailComponent} from './Pages/post-detail/post-detail.component';
-import {VideoComponent} from './Pages/video/video.component';
-import {LogDetailComponent} from './Pages/log-detail/log-detail.component';
-import {AlbumDetailComponent} from './Pages/album-detail/album-detail.component';
-import {AlbumComponent} from './Pages/album/album.component';
-import {PostComponent} from './Pages/post/post.component';
-import {PageComponent} from './Pages/page/page.component';
-import {MenuComponent} from './Layout/menu/menu.component';
-import {RestService} from './Services/rest.service';
-import {YoutubePlayerComponent} from './Components/youtube-player/youtube-player.component';
-import {FormsModule} from '@angular/forms';
-import {PostCreateComponent} from './Pages/post-create/post-create.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import { HomeComponent } from './Pages/home/home.component';
+import { LogComponent } from './Pages/log/log.component';
+import { PostDetailComponent } from './Pages/post-detail/post-detail.component';
+import { VideoComponent } from './Pages/video/video.component';
+import { LogDetailComponent } from './Pages/log-detail/log-detail.component';
+import { AlbumDetailComponent } from './Pages/album-detail/album-detail.component';
+import { AlbumComponent } from './Pages/album/album.component';
+import { PostComponent } from './Pages/post/post.component';
+import { PageComponent } from './Pages/page/page.component';
+import { MenuComponent } from './Layout/menu/menu.component';
+import { RestService } from './Services/rest.service';
+import { YoutubePlayerComponent } from './Components/youtube-player/youtube-player.component';
+import { FormsModule } from '@angular/forms';
+import { PostCreateComponent } from './Pages/post-create/post-create.component';
 import { TitleComponent } from './Components/title/title.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {PostEditComponent} from './Pages/post-edit/post-edit.component';
+import { PostEditComponent } from './Pages/post-edit/post-edit.component';
 import { PostListComponent } from './Components/post-list/post-list.component';
 import { AdminPanelComponent } from './Pages/admin-panel/admin-panel.component';
 import { IntroTextComponent } from './Components/intro-text/intro-text.component';
-import {ConfigComponent} from './Pages/config/config.component';
+import { ConfigComponent } from './Pages/config/config.component';
 import { SpinnerComponent } from './Components/spinner/spinner.component';
 import { CountTitleComponent } from './Components/count-title/count-title.component';
 import { AdminPageComponent } from './Pages/admin-page/admin-page.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { AuthenticationService } from './Services/authentication.service';
-
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -61,6 +59,8 @@ import { AuthenticationService } from './Services/authentication.service';
     LoginComponent
   ],
   imports: [
+    BrowserModule.withServerTransition({ appId: 'lifelike' }),
+
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
@@ -73,7 +73,15 @@ import { AuthenticationService } from './Services/authentication.service';
   ],
   exports: [
   ],
-  providers: [RestService, AuthenticationService  ],
+  providers: [RestService, AuthenticationService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
